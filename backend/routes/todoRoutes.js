f92afcea305b9b609196274d1f03cdbfd2766f96
shaharariel95 <shaharariel95@gmail.com>
2024-10-14 19:01:16 +0300

    added: Get todos Get todo/id post todo delete todo/id put todo\id -> update

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -1,8 +1,11 @@
-import { getTodos, addTodo } from '../controllers/todoController.js';
+import { getTodos, getTodoById, addTodo, updateTodo, deleteTodo } from '../controllers/todoController.js';
 
 async function todoRoutes(fastify, options) {
     fastify.get('/todos', getTodos);
+    fastify.get('/todos/:id', getTodoById);
     fastify.post('/todos', addTodo);
+    fastify.put('/todos/:id', updateTodo);
+    fastify.delete('/todos/:id', deleteTodo);
 }
 
 export default todoRoutes;
